a264c01b6c7abefe18d35dc161d9cda21ca4aeee
bnicholson87 <bnicholson091@gmail.com>
2021-03-03 14:28:53 -0500

    Updated connection.js and orm.js, added routes folder and files

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,24 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: '',
-  database: 'hockey_db',
-});
+require('dotenv').config();
+let connection ;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 8080,
+        user: 'root',
+        password: process.env.DB_PASSWORD,
+        database: 'hockey_db',
+    });
+}
 
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        throw err;
+    }
     console.log(`connected as id ${connection.threadId}\n`);
-  });
+});
 
-  module.exports = connection;
+module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-const connection = require("./connection.js")
+const connection = require('./connection.js')
 
 // DISPLAY Teams on each of their pages
 // ADD a player to player table using the registration page
@@ -9,7 +9,9 @@ const connection = require("./connection.js")
 const orm = {
     selectAll: function (player, cb) {
         connection.query(`SELECT * FROM ${player}`, (err, res) => {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
             cb(res);
         });
     },
@@ -18,35 +20,49 @@ const orm = {
             `INSERT INTO ${player} SET ?`,
             newPlayer,
             (err, res) => {
-                if (err) throw err;
+                if (err) {
+                    throw err;
+                }
                 console.log(`${res.affectedRows} Player added\n`);
                 cb(res);
             })
     },
+    newPlayer: function (firstName, lastName){
+        connection.query(
+            `INSERT INTO player VALUES (${firstName}, ${lastName})`, (err, res) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(res);
+            });
+    },
+
     editPlayerInfo: function (player, updateArray, cb) {
         const query = connection.query(
             `UPDATE ${player} SET ? WHERE ?`,
             updateArray,
             (err, res) => {
-              if (err) throw err;
-              console.log(`${res.affectedRows} Player info updated\n`);
-              cb(res);
+                if (err) {
+                    throw err;
+                }
+                console.log(`${res.affectedRows} Player info updated\n`);
+                cb(res);
             })
+    },
+
+    deletePlayer: function (player, condition, cb) {
+        let queryString = `DELETE FROM ${player}`;
+        queryString += ' WHERE ';
+        queryString += condition;
+
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     }
 }
-
-deletePlayer(player, condition, cb) {
-    let queryString = `DELETE FROM ${player}`;
-    queryString += ' WHERE ';
-    queryString += condition;
-
-    connection.query(queryString, (err, result) => {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-};
 
 module.exports = orm;

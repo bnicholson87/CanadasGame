f74909beddbea028027c0a573d0df2118c8b6424
daze_77 <daze77@gmail.com>
2021-03-03 14:34:57 -0500

    updating

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require( 'dotenv' ).config() // looks for .env ; process.env gets it's values
 
+var htmlroutes = require('./routes/htmlroutes.js')
+var apiroutes = require('./routes/apiroutes.js')
+
 const express = require('express')
 const apiRouter = require('./app/router')
 const app = express()
@@ -13,6 +16,7 @@ app.use(express.json())
 // for serving all the normal html
 app.use( express.static('public') )
 
+// require('./routes/apiroutes')(app)
 // for routes
 apiRouter(app)
 

f8ac761c34af5da3a1308348b0b88d4bb557ce1c
bnicholson87 <bnicholson091@gmail.com>
2021-03-05 15:40:08 -0500

    Made one little change to schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE player (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (100) NOT NULL,
     last_name VARCHAR (100) NOT NULL,
-    birth_date DATE NOT NULL,
+    birth_date VARCHAR (20) NOT NULL,
     email VARCHAR (100) NOT NULL,
     street VARCHAR (100) NOT NULL,
     city VARCHAR (100) NOT NULL,
